616751cd524a967ea74cea1b0b9451f32bf75b79
Wose <Jacksonteather@hotmail.com>
2017-05-29 09:38:35 -0400

    🐛 Mitigate goal field highlighting oddness

--- src/calculator/goal/goal.component.js
+++ src/calculator/goal/goal.component.js
@@ -7,6 +7,16 @@ const controller = class GoalController {
     this.goal = 0;
   }
 
+  $onInit() {
+    this.goal = this.healthyAverage;
+  }
+
+  get healthyAverage() {
+    const gradeTotal = (this.grades.reduce((acc, grade) => (acc += grade.mark), 0));
+    const gradeAverage = gradeTotal / this.grades.length;
+    return gradeAverage ? this.round(gradeAverage, 2) : 80;
+  }
+
   get percentNeeded() {
     const gradeAdjusted = this.grades.reduce((acc, grade) => (acc += grade.adjusted), 0);
     const percentNeeded = (this.goal - gradeAdjusted) / (this.valueRemaining / 100);
